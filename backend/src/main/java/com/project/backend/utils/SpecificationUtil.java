3f1a23469bff51107f461b356dc70e8041840fb5
Artem <ma.ua@outlook.com>
2025-07-02 23:07:28 +0300

    Hotfix

--- backend/src/main/java/com/project/backend/utils/SpecificationUtil.java
+++ backend/src/main/java/com/project/backend/utils/SpecificationUtil.java
@@ -3,6 +3,7 @@ package com.project.backend.utils;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class SpecificationUtil {
     public static boolean isValid(String value) {
@@ -11,6 +12,10 @@ public class SpecificationUtil {
 
     public static <T> Specification<T> addSpecification(Specification<T> specification, Function<String, Specification<T>> addFunction, String value) {
         Specification<T> newSpec = addFunction.apply(value);
+        return (specification == null) ? newSpec : specification.and(newSpec);
+    }
+    public static <T> Specification<T> addSpecification(Specification<T> specification, Supplier<Specification<T>> addFunction) {
+        Specification<T> newSpec = addFunction.get();
         return (specification == null) ? newSpec : specification.and(newSpec);
     }
 }

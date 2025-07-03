968cefb5a55cf5b661b4dda1b7623a86b80e68a1
Artem <ma.ua@outlook.com>
2025-07-03 18:56:37 +0300

    Add inserts for user

---
+++ backend/src/main/resources/db/migration/V004__insert_user.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (first_name, last_name, email, keycloak_user_id, role, school_id)
+VALUES ('Ivan', 'Ivanov', 'ivan@example.com', '', 'PARENT', 1),
+       ('Maria', 'Petrenko', 'maria@example.com', '', 'TEACHER', 1),
+       ('Oleh', 'Sydorenko', 'oleh@example.com', '', 'STUDENT', 1),
+       ('Andrii', 'Melnyk', 'andrii.melnyk@example.com', 'kc_andrii_teacher', 'PARENT', 1),
+       ('Iryna', 'Bondarenko', 'iryna.bond@example.com', 'kc_iryna_teacher', 'TEACHER', 1),
+       ('Taras', 'Shevchenko', 'taras.sh@example.com', 'kc_taras_student', 'STUDENT', 1),
+       ('Lesya', 'Ukrainka', 'lesya.ukr@example.com', 'kc_lesya_student', 'PARENT', 1),
+       ('Ivan', 'Franko', 'ivan.fr@example.com', 'kc_ivan_student', 'STUDENT', 1),
+       ('Kateryna', 'Bilokur', 'kateryna.b@example.com', 'kc_kateryna_student', 'STUDENT', 1),
+       ('Mykhailo', 'Hrushevsky', 'mykhailo.h@example.com', 'kc_mykhailo_student', 'STUDENT', 1);

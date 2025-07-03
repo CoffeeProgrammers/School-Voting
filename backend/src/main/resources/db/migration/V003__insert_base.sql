c907aa07ad5100c99d87878047878e5dabeba395
Artem <ma.ua@outlook.com>
2025-07-03 15:14:59 +0300

    Add inserts

---
+++ backend/src/main/resources/db/migration/V003__insert_base.sql
@@ -0,0 +1,12 @@
+INSERT INTO "users" ("email", "keycloak_user_id", "role", "first_name", "last_name")
+VALUES ('bulakovskijvladislav@gmail.com', '95c06363-2168-4108-bf5b-d76bf8a8e31b',
+        'TEACHER', 'Bulakovski', 'Vladyslav'),
+       ('!deleted-user!@deleted.com', '1234567890abcdABCD0987654321',
+        'DELETED', 'Deleted', 'User');
+
+INSERT INTO schools (name, director_id)
+VALUES ('Greenwood High School', (SELECT id FROM users WHERE email = 'bulakovskijvladislav@gmail.com'));
+
+UPDATE users
+SET school_id = (SELECT id FROM schools WHERE name = 'Greenwood High School')
+WHERE email IN ('bulakovskijvladislav@gmail.com', '!deleted-user!@deleted.com');

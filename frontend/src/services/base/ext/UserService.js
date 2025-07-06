2678236ed772b9be9cbb9d4715353ebfc5c6b3bc
Yaroslav Holobyn <slavaholobyn@gmail.com>
2025-07-06 22:13:58 +0300

    create services

---
+++ frontend/src/services/base/ext/UserService.js
@@ -0,0 +1,69 @@
+import BaseService from "../BaseService";
+
+class UserService extends BaseService {
+    constructor() {
+        super(`/schools/${1}/users`);
+    }
+
+    createUser(data) {
+        return this.post("/create", data);
+    }
+
+    updateUser(userId, data) {
+        return this.put(`/update/${userId}`, data);
+    }
+
+    deleteUser(userId) {
+        return this.delete(`/delete/${userId}`);
+    }
+
+    getMyUser() {
+        return this.get("/my");
+    }
+
+    getUsersByVoting(votingId, {email, firstName, lastName, page, size}) {
+        const params = {
+            ...(email && {email}),
+            ...(firstName && {firstName}),
+            ...(lastName && {lastName}),
+            page,
+            size
+        };
+        return this.get(`/voting/${votingId}`, {params});
+    }
+
+    getUsersByRole(role, {email, firstName, lastName, page, size}) {
+        const params = {
+            ...(email && {email}),
+            ...(firstName && {firstName}),
+            ...(lastName && {lastName}),
+            page,
+            size
+        };
+        return this.get(`/role/${role}`, {params});
+    }
+
+    getUsersByClass(classId, {email, firstName, lastName, page, size}) {
+        const params = {
+            ...(email && {email}),
+            ...(firstName && {firstName}),
+            ...(lastName && {lastName}),
+            page,
+            size
+        };
+        return this.get(`/class/${classId}`, {params});
+    }
+
+    getUsersWithoutClass({email, firstName, lastName, page, size}) {
+        const params = {
+            ...(email && {email}),
+            ...(firstName && {firstName}),
+            ...(lastName && {lastName}),
+            page,
+            size
+        };
+        return this.get(`/withoutClass`, {params});
+    }
+}
+
+export default new UserService();

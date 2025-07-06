9ded4bc71574794f24e41a6a7bbb1bc92855e19c
Vladobrod <bulakovskijvladislav@gmail.com>
2025-07-07 00:17:57 +0300

    updated TestUtil

--- backend/src/test/java/com/project/backend/TestUtil.java
+++ backend/src/test/java/com/project/backend/TestUtil.java
@@ -1,7 +1,10 @@
 package com.project.backend;
 
+import com.project.backend.models.School;
 import com.project.backend.models.User;
 import org.instancio.Instancio;
+
+import java.util.Arrays;
 
 import static org.instancio.Select.field;
 
@@ -11,5 +14,27 @@ public class TestUtil {
         user.setRole(role);
         user.setEmail(email);
         return user;
+    }
+
+    public static User createUser(String role, String email, String keycloakUserId) {
+        User user = createUser(role, email);
+        user.setKeycloakUserId(keycloakUserId);
+        return user;
+    }
+
+    public static School createSchool(String name) {
+        School school = Instancio.of(School.class).ignore(field("name")).create();
+        school.setName(name);
+        return school;
+    }
+
+    public static School createSchool(String name, User director) {
+        School school = createSchool(name);
+        school.setDirector(director);
+        return school;
+    }
+
+    public static void assignUsersToSchool(School school, User... users) {
+        Arrays.stream(users).forEach(u -> u.setSchool(school));
     }
 }
